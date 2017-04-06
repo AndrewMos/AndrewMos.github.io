60166611d1ac11157fa06b1761f3cc4738a5143b
AndrewMos <moskovecandrey@gmail.com>
2017-04-06 23:46:55 +0300

    Update game2.js

--- game2.js
+++ game2.js
@@ -2,7 +2,20 @@ var k;
 var col = 6;
 row = 8;
 
+var slider1, slider2;
+
 function setup() {
+  //createve
+  slider1 = createSlider(1, 30, 6, 1);
+  slider1.position(10, 10);
+  slider1.style('width', '100px');
+  slider2 = createSlider(1, 30, 8, 1);
+  slider2.position(10, 30);
+  slider2.style('width', '100px');
+  //end
+  
+  
+  
 	createCanvas(windowWidth,windowHeight);
 		if (windowWidth > windowHeight) {
 			k = (windowHeight/row)*0.8;
@@ -18,6 +31,24 @@ function setup() {
 }
 
 function draw() {
+  //creative
+    col = slider1.value();
+    row = slider2.value();
+    	createCanvas(windowWidth,windowHeight);
+		if (windowWidth > windowHeight) {
+			k = (windowHeight/row)*0.8;
+			cnv = createCanvas(k*col, k*row);
+			cnv.position(windowWidth/2-k*col/2, 0);
+		cnv.background(50);
+	} else {
+			k = (windowWidth/col)*0.8;
+			cnv = createCanvas(k*col, k*row);
+			cnv.position(windowWidth/2-k*col/2, 0);
+		cnv.background(240);
+	}
+  //end
+  
+  
 	stroke(200,200,200);
 	
 	   for(var i = 1; i < col; i++) {
@@ -61,3 +92,5 @@ function windowResized() {
 		cnv.background(240);
 	}
 }
+
+
