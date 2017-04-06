3501d6c2a23a0cb955d3a5318ef22202759dfeed
AndrewMos <moskovecandrey@gmail.com>
2017-04-06 21:34:34 +0300

    Update game2.js

--- game2.js
+++ game2.js
@@ -2,13 +2,19 @@ var k;
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
-	k = windowWidth/6;
-	cnv = createCanvas(k*6, k*8);
-	cnv.position(0, 0);
-	
+		if (windowWidth > windowHeight)
+	{
+			k = windowHeight/8;
+			cnv = createCanvas(k*6, k*8);
+			cnv.position(0, 0);
+	} else
+	{
+			k = windowWidth/6;
+			cnv = createCanvas(k*6, k*8);
+			cnv.position(0, 0);
+	}
 }
 
 function draw() {
 	cnv.background(200);
 }
-
