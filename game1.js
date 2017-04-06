f53aaf496cf36117af101acd01151d3cf50a18d7
AndrewMos <moskovecandrey@gmail.com>
2017-04-06 13:24:49 +0300

    Update game1.js

--- game1.js
+++ game1.js
@@ -24,7 +24,7 @@ if (windowWidth < windowHeight) {
    } else {
       wid = 500;
       hei = 600;
-      wid0 = windowWidth/2 - wid;
+      wid0 = windowWidth/2;
       hei0 = 0;
       k = (windowHeight / 500) * 0.7;
    }
