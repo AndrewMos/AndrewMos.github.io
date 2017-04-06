7317506a04770983385700fef4f37a4c80946f34
AndrewMos <moskovecandrey@gmail.com>
2017-04-06 22:46:26 +0300

    Update game2.js

--- game2.js
+++ game2.js
@@ -20,7 +20,7 @@ function setup() {
 function draw() {
 	stroke(200,200,200);
 	
-	    for(var i = 1; i < 6; i++)
+	   for(var i = 1; i < 6; i++)
 	{
 	    line(i*k, 0, i*k, k*8);
 	}
@@ -28,6 +28,21 @@ function draw() {
 	{
 	    line(0, i*k, k*6, k*i);
 	}
-	
-	
+}
+
+
+function windowResized() {
+  	if (windowWidth > windowHeight)
+	{
+			k = (windowHeight/8)*0.8;
+			cnv = createCanvas(k*6, k*8);
+			cnv.position(0, 0);
+		cnv.background(50);
+	} else
+	{
+			k = (windowWidth/6)*0.8;
+			cnv = createCanvas(k*6, k*8);
+			cnv.position(0, 0);
+		cnv.background(240);
+	}
 }
