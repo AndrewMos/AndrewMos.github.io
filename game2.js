9c85af694bba379cbd0e99dd8edc27ad4d2cc081
AndrewMos <moskovecandrey@gmail.com>
2017-04-06 13:30:30 +0300

    Update game2.js

--- game2.js
+++ game2.js
@@ -1,9 +1,22 @@
-
+var k;
 
 function setup() {
-
+	createCanvas(windowWidth,windowHeight);
+	k = windowWidth/6;
+	cnv = createCanvas(k*6, k*8);
+	cnv.position(0, 0);
+	
 }
 
 function draw() {
+	cnv.background(200);
+}
+
+
+function Block() {
+
+this.function = move() {
+
+}
 
 }
